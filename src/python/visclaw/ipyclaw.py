81460bea080860361f9f57a37d4f673e4a4b75a7
Kyle Mandli <kyle.mandli@gmail.com>
2013-06-07 17:41:06 -0500

    Add simple convenience script
    Adds a simple script that can be then put on the path for easy interactive plotting.

---
+++ src/python/visclaw/ipyclaw.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+r"""
+Simple convenience script for setting up and running an interactive plotting
+sesssion.
+"""
+
+import sys
+import clawpack.visclaw.Iplotclaw as Iplotclaw
+
+def run_iplotclaw(setplot='setplot.py',outdir='./_output'):
+    ip = Iplotclaw.Iplotclaw(setplot=setplot,outdir=outdir)
+    ip.plotloop()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        run_iplotclaw(*sys.argv[1:])
+    else:
+        run_iplotclaw()
