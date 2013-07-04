0d4d68c9e303494e0070c67b60d1c55dff14c8ee
Randy LeVeque <rjl@ned>
2013-07-04 11:01:25 -0700

    Need to debug why gauge.number is not set properly - added comments at relevant points

--- src/python/visclaw/data.py
+++ src/python/visclaw/data.py
@@ -330,6 +330,7 @@ class ClawPlotData(clawdata.ClawData):
                         gauge.level = [int(value) for value in raw_data[gauge_indices,1]]
                         gauge.t = raw_data[gauge_indices,2]
                         gauge.q = raw_data[gauge_indices,3:].transpose()
+                        gauge.number = gaugeno
                         gauge_read_string = " ".join((gauge_read_string,str(n)))
 
                         self.gaugesoln_dict[(n, outdir)] = gauge
@@ -344,6 +345,8 @@ class ClawPlotData(clawdata.ClawData):
         else:
             # Attempt to fetch gauge requested
             gauge = self.gaugesoln_dict[key]
+            # Need to debug why gauge.number is not set properly...
+            #print "gaugeno = %s and gauge.number = %s" % (gaugeno, gauge.number)
 
         return gauge
 
