e5c0214abc7190b2107fbed03d5e82dbac86c59b
spotter0 <stephaniepotter@u.boisestate.edu>
2014-02-13 13:52:46 -0700

    Changes to file

--- src/kml/plotclaw2kml.py
+++ src/kml/plotclaw2kml.py
@@ -2,6 +2,8 @@
 # The kml will then be created, containing all data (placemarks, metadata, 
 # images, etc.) to be loaded into Google Earth
 
+# See visclaw/src/python/visclaw/plotpages.py for template for this file
+
 pm1 = KML.Placemark(
     KML.name("Hello World!"),
     KML.Point(
