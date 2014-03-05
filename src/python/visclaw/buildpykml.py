583d74e60704688128ff6637239e1ac56f95abba
spotter0 <stephaniepotter@u.boisestate.edu>
2014-03-05 12:09:56 -0700

    Error message added, some issued with parser file I believe..?

--- src/python/visclaw/buildpykml.py
+++ src/python/visclaw/buildpykml.py
@@ -1,9 +1,19 @@
 import pykml
+#import urllib2
+
+#try:
+#  print urllib2.urlopen('http://www.reefgeek.com/equipment/Controllers_&_Monitors/Neptune_Systems_AquaController/Apex_Controller_&_Accessories/').read()
+#except urllib2.HTTPError, e:
+#    print e.code
+#    print e.msg
+#    print e.headers
+#    print e.fp.read()
+
 from pykml import parser
 
 from pykml.factory import write_python_script_for_kml_document
 
-url = 'http://code.google.com/apis/kml/documentation/kmlfiles/altitudemode_reference.kml'
+#url = 'http://code.google.com/apis/kml/documentation/kmlfiles/altitudemode_reference.kml'
 
 #fileobject = urllib2.urlopen(url)
 fileobject = open('file.kml')
@@ -17,3 +27,19 @@ fileout=open('file.py','w')
 fileout.write(script)
 
 fileout.close()
+
+#Traceback (most recent call last):
+#  File "buildpykml.py", line 21, in <module>
+#    doc = parser.parse(fileobject).getroot()
+#  File "/Users/spotter0/anaconda/lib/python2.7/site-packages/pykml/parser.py", #line 55, in parse
+#    return objectify.parse(fileobject)
+#  File "lxml.objectify.pyx", line 1850, in lxml.objectify.parse (src/lxml/lxml.#objectify.c:21435)
+#  File "lxml.etree.pyx", line 3197, in lxml.etree.parse (src/lxml/lxml.etree.c:#65042)
+#  File "parser.pxi", line 1593, in lxml.etree._parseDocument (src/lxml/lxml.etr#ee.c:93318)
+#  File "parser.pxi", line 1624, in lxml.etree._parseFilelikeDocument (src/lxml/#lxml.etree.c:93661)
+#  File "parser.pxi", line 1506, in lxml.etree._parseDocFromFilelike (src/lxml/l#xml.etree.c:92516)
+#  File "parser.pxi", line 1069, in lxml.etree._BaseParser._parseDocFromFilelike# (src/lxml/lxml.etree.c:89538)
+#  File "parser.pxi", line 577, in lxml.etree._ParserContext._handleParseResultD#oc (src/lxml/lxml.etree.c:84711)
+#  File "parser.pxi", line 676, in lxml.etree._handleParseResult (src/lxml/lxml.#etree.c:85816)
+#  File "parser.pxi", line 616, in lxml.etree._raiseParseError (src/lxml/lxml.etree.c:85138)
+#lxml.etree.XMLSyntaxError: Start tag expected, '<' not found, line 1, column 1
