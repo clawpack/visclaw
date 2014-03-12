b86a76f2a969c85e3ec3a2369f0f81ef3c83a7ec
David Ketcheson <dketch@gmail.com>
2014-03-12 20:23:53 +0300

    Add ianimate function for JSAnimations in the notebook.

---
+++ src/python/visclaw/ianimate.py
@@ -0,0 +1,49 @@
+"""
+Interactive animations in IPython notebooks using matplotlib and JSAnimation.
+"""
+
+
+def ianimate(frame_list,plotdata=None,**kargs):
+    """
+        frame_list may be:
+
+            - a list of Solution objects
+            - a controller possessing a list of Solution objects
+            - a string specifying a relative path to a set of output files
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+    from clawpack.visclaw.JSAnimation import IPython_display
+    from clawpack.pyclaw import Controller
+
+    if isinstance(framelist,Controller):
+        framelist = framelist.frames
+
+    ivar = 0
+
+    frame = framelist[0]
+    ndim = len(frame.q.shape)-1
+    if ndim == 1:
+        fig = plt.figure(figsize=(8,4))
+        ax = plt.axes()
+        xc = frame.state.grid.p_centers
+        im, = ax.plot([], [])
+    elif ndim == 2:
+        fig = plt.figure(figsize=(4,4))
+        xc, yc = frame.state.grid.p_centers
+        im = plt.imshow(frame.q[ivar,:,:].T,
+                extent = [xc.min(), xc.max(), yc.min(), yc.max()], 
+                interpolation='nearest',origin='lower')
+
+
+    def fplot(frame_number):
+        frame = framelist[frame_number]
+        if ndim == 1:
+            im.set_data(xc, frame.q[ivar,:])
+        elif ndim == 2:
+            im.set_data(frame.q[ivar,:,:].T)
+        else:
+            raise Exception('3D animation not yet implemented')
+        return im,
+
+    return animation.FuncAnimation(fig, fplot, frames=len(framelist))
