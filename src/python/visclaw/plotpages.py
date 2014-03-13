9ef5a0ec25a9cab5c6d838c0000337e3530e0c38
spotter0 <stephaniepotter@u.boisestate.edu>
2014-03-12 21:11:07 -0600

    Added final time stamp

--- src/python/visclaw/plotpages.py
+++ src/python/visclaw/plotpages.py
@@ -602,8 +602,7 @@ def plotclaw2kml(plotdata):
 
 
 #------------------STEPH-------------------
-# So far, no successful changes. I cannot
-# seem to write to googleearth.kml
+# Working so far!
 #------------------------------------------
     doc = KML.kml(
     KML.Document(
@@ -639,6 +638,25 @@ def plotclaw2kml(plotdata):
                     KML.rotation(0.0)) 
                 )
         )
+        
+    doc.Document.Folder.append(
+        KML.GroundOverlay(
+            KML.TimeSpan(
+                KML.begin(2013-10-02T09:00:00Z),
+                KML.end(2013-10-02T09:00:00Z)),
+            KML.drawOrder(i),
+            KML.altitude(0.0),
+            KML.altitudeMode("clampToGround"),
+            KML.Icon(
+                KML.href(fname)),
+            KML.LatLonBox(
+                KML.north(0.0),
+                KML.south(-60.0),
+                KML.east(-60.0),
+                KML.west(-120.0),
+                KML.rotation(0.0)) 
+        )
+    )
         
 
     filekml.write(etree.tostring(etree.ElementTree(doc),pretty_print=True))
