e428d3fee9e29ccfa6439a0b9179d547c6b15631
donnaaboise <donna.calhoun@gmail.com>
2015-10-26 11:32:25 -0700

    Check against lower(arg)

--- src/matlab/setopengl.m
+++ src/matlab/setopengl.m
@@ -29,5 +29,5 @@ end;
 if (~found_opengl)
   disp('*** Warning : The OpenGL renderer is not available on your system.');
 else
-  set(gcf,'Renderer','OpenGL');
+  set(gcf,'Renderer','opengl');
 end;
