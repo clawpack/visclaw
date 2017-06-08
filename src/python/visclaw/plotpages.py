7a613f3d7f58382fe0d5cb04651e1befad0b53c2
donnaaboise <donna.calhoun@gmail.com>
2017-06-08 23:48:28 +0200

    (KML) Change to plotfigure attribute"

--- src/python/visclaw/plotpages.py
+++ src/python/visclaw/plotpages.py
@@ -1051,7 +1051,7 @@ def plotclaw2kml(plotdata):
 
             # ExtendedData is used in BalloonStyle.text() fields.
             placemark = KML.Placemark(
-                KML.name("%s %d" % (plotdata.kml_gauge_name,gaugeno)),
+                KML.name("%s %d" % (plotfigure.kml_gauge_name,gaugeno)),
                 KML.Snippet(snippet_str),
                 KML.styleUrl(chr(35) + "gauge_style"),
                 KML.ExtendedData(
