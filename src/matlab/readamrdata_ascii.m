d245d56cd2ee27620a39a1bb2b793fef48cd3a6f
donnaaboise <donna.calhoun@gmail.com>
2018-12-07 14:23:43 -0700

    Minor formatting updates

--- src/matlab/readamrdata_ascii.m
+++ src/matlab/readamrdata_ascii.m
@@ -1,4 +1,4 @@
-function [amr,t] = readamrdata_ascii(dim,Frame,dir,readblocknumber);
+function [amr,t] = readamrdata_ascii(dim,Frame,dir,readblocknumber)
 
 % Internal routine for Clawpack graphics.
 
@@ -6,7 +6,7 @@ n1 = Frame+10000;
 fname = [dir, 'fort.',num2str(n1)];
 fname(length(dir)+6) = 't';
 
-if ~exist(fname)
+if ~exist(fname,'file')
   amr = {};
   t = [];
   disp(' ');
@@ -18,7 +18,6 @@ end
 % Read data from fname = 'fort.tXXXX'
 fid = fopen(fname);
 
-
 t = fscanf(fid,'%g',1);        fscanf(fid,'%s',1);
 meqn = fscanf(fid,'%d',1);     fscanf(fid,'%s',1);
 ngrids = fscanf(fid,'%d',1);   fscanf(fid,'%s',1);
@@ -26,7 +25,7 @@ fclose(fid);
 
 % change the file name to read the q data:
 fname(length(dir) + 6) = 'q';
-if ~exist(fname)
+if ~exist(fname,'file')
   amr = {};
   t = [];
   return;
@@ -35,7 +34,8 @@ end
 fid = fopen(fname);
 disp(['Reading data from ',fname]);
 
-for ng = 1:ngrids,
+amr = zeros(ngrids,1);
+for ng = 1:ngrids
 
   % read parameters for this grid:
 
@@ -53,24 +53,24 @@ for ng = 1:ngrids,
     amrdata.my = fscanf(fid,'%d',1);     fscanf(fid,'%s',1);
     if (dim > 2)
       amrdata.mz = fscanf(fid,'%d',1);     fscanf(fid,'%s',1);
-    end;
-  end;
+    end
+  end
 
   amrdata.xlow = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
   if (dim > 1)
     amrdata.ylow = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
     if (dim > 2)
       amrdata.zlow = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
-    end;
-  end;
+    end
+  end
 
   amrdata.dx = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
   if (dim > 1)
     amrdata.dy = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
     if (dim > 2)
       amrdata.dz = fscanf(fid,'%g',1);     fscanf(fid,'%s',1);
-    end;
-  end;
+    end
+  end
 
   % read q data:
   if (dim == 1)
@@ -79,7 +79,7 @@ for ng = 1:ngrids,
     amrdata.data = fscanf(fid,'%g',[meqn,amrdata.mx*amrdata.my]);
   else
     amrdata.data = fscanf(fid,'%g',[meqn,amrdata.mx*amrdata.my*amrdata.mz]);
-  end;
+  end
 
   amr(ng) = amrdata;
 
