3c151bbf8fe1e732120165413a05fca3348cdce9
Randy LeVeque <rjl@uw.edu>
2020-09-22 13:39:03 -0700

    change mapped_grid_exists to mapc2p_exists

--- src/python/visclaw/frametools.py
+++ src/python/visclaw/frametools.py
@@ -191,15 +191,15 @@ def plot_frame(framesolns,plotdata,frameno=0,verbose=False):
             if skip_patches_outside_xylimits is None:
                 # User didn't set.  Set to True unless there's a mapped grid
                 
-                mapped_grid_exists = (plotdata.mapc2p is not None)
-                if not mapped_grid_exists:
+                mapc2p_exists = (plotdata.mapc2p is not None)
+                if not mapc2p_exists:
                     # check every item in case there's a mapc2p:
                     for itemname in plotaxes._itemnames:
                         plotitem = plotaxes.plotitem_dict[itemname]
-                        mapped_grid_exists = mapped_grid_exists or \
+                        mapc2p_exists = mapc2p_exists or \
                                            (plotitem.mapc2p is not None)
                                            
-                skip_patches_outside_xylimits = not mapped_grid_exists
+                skip_patches_outside_xylimits = not mapc2p_exists
                 
 
             # NOTE: This was rearranged December 2009 to
